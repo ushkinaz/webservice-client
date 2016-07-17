8a754bf98d82147624f317e3ae5afa9b1bf6af80
Dmitry Sidorenko <rustydragon@gmail.com>
2016-07-17 13:18:53 +0300

    Initial

---
+++ src/main/java/ru/sid0renk0/webtest/WebServDemoApplication.java
@@ -0,0 +1,12 @@
+package ru.sid0renk0.webtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/sid0renk0/webtest/WebServDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.sid0renk0.webtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebServDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
